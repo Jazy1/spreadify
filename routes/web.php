f21ff1bb818120dcb99d2f24b8d4b776a7904d37
Jazy1 <90447913+Jazy1@users.noreply.github.com>
2022-12-05 17:08:30 +0500

    Commit #2

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/success', function (Request $request) {
     return $request->code;
 });
 
-Route::get("auth/ebay/get-application-access-token", function(Request $request){
+Route::get("auth/ebay/get-application-access-token", function(){
     
     $link = "https://api.sandbox.ebay.com/identity/v1/oauth2/token";
     $clientId = env("EBAY_CLIENT_ID");
@@ -34,15 +34,40 @@ Route::get("auth/ebay/get-application-access-token", function(Request $request){
     $b64 = base64_encode($clientId.":".$clientSecert);
     $encodedScope = urlencode($scope);
 
-    $response = Http::withHeaders([
-        "Content-Type" => "application/x-www-form-urlencoded",
-        "Authorization" => "Basic $b64"
-    ])->post($link, [
+    $response = Http::dd()->withHeaders([
+        "Authorization" => "Basic SmFoYW56ZWItc3ByZWFkaWYtU0JYLTJjNTk4ZjEzMC02YTQ4OTE4YzpTQlgtYzU5OGYxMzAxMmNkLTJkZTAtNDVlZi04MTNhLWYwNmQ="
+    ])->asForm()->post("https://api.sandbox.ebay.com/identity/v1/oauth2/token", [
         "grant_type" => "client_credentials",
         "scope" => $encodedScope
     ]);
 
-    return $response->status();
+    // return $response->status();
+    return $response->body();
     // print_r($response);
     // echo("musha");
+
+    // $curl = curl_init();
+
+    // curl_setopt_array($curl, array(
+    //   CURLOPT_URL => 'https://api.sandbox.ebay.com/identity/v1/oauth2/token',
+    //   CURLOPT_RETURNTRANSFER => true,
+    //   CURLOPT_ENCODING => '',
+    //   CURLOPT_MAXREDIRS => 10,
+    //   CURLOPT_TIMEOUT => 0,
+    //   CURLOPT_FOLLOWLOCATION => true,
+    //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //   CURLOPT_CUSTOMREQUEST => 'POST',
+    //   CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=https%3A%2F%2Fapi.ebay.com%2Foauth%2Fapi_scope',
+    //   CURLOPT_HTTPHEADER => array(
+    //     'Authorization: Basic SmFoYW56ZWItc3ByZWFkaWYtU0JYLTJjNTk4ZjEzMC02YTQ4OTE4YzpTQlgtYzU5OGYxMzAxMmNkLTJkZTAtNDVlZi04MTNhLWYwNmQ=',
+    //     'Content-Type: application/x-www-form-urlencoded',
+        
+    //   ),
+    // ));
+    
+    // $response = curl_exec($curl);
+    
+    // curl_close($curl);
+    // echo $response;
+
 });
